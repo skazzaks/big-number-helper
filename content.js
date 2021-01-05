84a9d95b394749760f85a406c2624b7ddfabbabe
Devon Fritz <devon.s.fritz@gmail.com>
2021-01-05 14:52:06 +0100

    first working version

--- content.js
+++ content.js
@@ -7,20 +7,25 @@
 //         var node = element.childNodes[j];
 
 //         if (node.nodeType === 3) {
-//             var origText = node;
-//             var text = origText;
+//             var text = node.nodeValue;
+//             var replacedText = text.replace(/([\$])?([0-9.]+)\s?(billion|bn)/gi, '<span style="color:' + 'red' + '">$1$2 million!!xx</span>');
 
-//             text = text.replace(/([0-9]+)bn/gi, "$1,000,000,000"); // Note this worked for expanding the number
-//             text = text.replace(/([0-9]+)trn/gi, "$1,000,000,000,000");
-//             if (text !== origText) {
-//                 element.replaceChild(document.createTextNode(text), node);
+//             if (replacedText !== text) {
+//                 element.replaceChild(document.createTextNode(replacedText), node);
 //             }
-            
 //         }
 //     }
 // }
 
+
 var color = "red";
-document.body.innerHTML = document.body.innerHTML.replace(/([\$])?([0-9.]+)\s?(million|ml)/gi, '<span style="color:' + color + '">$1$2 million</span>')
-    .replace(/([\$])?([0-9.]+)\s?(billion|bn)/gi, '<span style="color:' + color + '">$1$2 billion</span>')
-    .replace(/([\$])?([0-9.]+) \s?(trillion|tn)/gi, '<span style="color:' + color + '">$1$2 trillion</span>')
+
+var elements = document.getElementsByTagName('p');
+for (var i = 0; i < elements.length; i++) {
+    element = elements[i];
+
+element.innerHTML = element.innerHTML.replace(/([\$])([0-9.]+)(\s?million|m)/gi, '<span style="color:' + color + '">$1$2 million (You could buy with: $$1 million: Huge house in suburbs; nice condo downtown in large city | $$10 million: A small hospital | $$100 million: Large city office building)</span>')
+    .replace(/([\$])([0-9.]+)\s?(billion|bn)/gi, '<span style="color:' + color + '">$1$2 billion (You could buy with: $$1 billion: Burj Khalifa, world\'s tallest building | $$55 billion: Cost of a human mission to Mars with a crew of four | $$299 billion: GDP of Greece)</span>')
+    .replace(/([\$])([0-9.]+) \s?(trillion|tn)/gi, '<span style="color:' + color + '">$1$2 trillion (You could buy with: $$4.79 trillion: United States annual federal budget for 2020 | $$19.49 billion: US GDP in 2017 | $$100 trillion: Global GDP in 2017)</span>')
+}
+
