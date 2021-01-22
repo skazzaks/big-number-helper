36b91b6e3f5ed4313be3a5f1854bcb7bea517dec
Devon Fritz <devon.s.fritz@gmail.com>
2021-01-22 12:09:32 +0100

    added icon and fixed bug with some numbers not being found

--- content.js
+++ content.js
@@ -1,31 +1,31 @@
-// var elements = document.getElementsByTagName('*');
-
-// for (var i = 0; i < elements.length; i++) {
-//     var element = elements[i];
-
-//     for (var j = 0; j < element.childNodes.length; j++) {
-//         var node = element.childNodes[j];
-
-//         if (node.nodeType === 3) {
-//             var text = node.nodeValue;
-//             var replacedText = text.replace(/([\$])?([0-9.]+)\s?(billion|bn)/gi, '<span style="color:' + 'red' + '">$1$2 million!!xx</span>');
-
-//             if (replacedText !== text) {
-//                 element.replaceChild(document.createTextNode(replacedText), node);
-//             }
-//         }
-//     }
-// }
-
-
 var color = "red";
+var millionText = '<span style="color:' + color + '"><b>$1$2 million</b> (You could buy with: $$1 million: Huge house in suburbs; nice condo downtown in large city | $$10 million: A small hospital | $$100 million: Large city office building | $$450 million: An Airbus A380, the world\'s largest passenger airliner)</span>';
+var billionText = '<span style="color:' + color + '"><b>$1$2 billion</b> (You could buy with: $$1 billion: Burj Khalifa, world\'s tallest building | $$55 billion: Cost of a human mission to Mars with a crew of four | $$299 billion: GDP of Greece)</span>';
+var trillionText = '<span style="color:' + color + '"><b>$1$2 trillion</b> (You could buy with: $$2 trillion: Apple, the world\'s largest company | $$4.79 trillion: United States annual federal budget for 2020 | $$19.49 trillion: US GDP in 2017 | $$100 trillion: Global GDP in 2017)</span>';
 
-var elements = document.getElementsByTagName('p');
-for (var i = 0; i < elements.length; i++) {
-    element = elements[i];
+var millionText2 = '<span style="color:' + color + '"><b>$1 million $3</b> (You could buy with: $$1 million: Huge house in suburbs; nice condo downtown in large city | $$10 million: A small hospital | $$100 million: Large city office building | $$450 million: An Airbus A380, the world\'s largest passenger airliner)</span>)</span>';
+var billionText2 = '<span style="color:' + color + '"><b>$1 billion $3</b> (You could buy with: $$1 billion: Burj Khalifa, world\'s tallest building | $$55 billion: Cost of a human mission to Mars with a crew of four | $$299 billion: GDP of Greece)</span>';
+var trillionText2 = '<span style="color:' + color + '"><b>$1 trillion $3</b> (You could buy with: $$2 trillion: Apple, the world\'s largest company | You could buy with: $$4.79 trillion: United States annual federal budget for 2020 | $$19.49 trillion: US GDP in 2017 | $$100 trillion: Global GDP in 2017)</span>';
 
-element.innerHTML = element.innerHTML.replace(/([\$])([0-9.]+)(\s?million|m)/gi, '<span style="color:' + color + '">$1$2 million (You could buy with: $$1 million: Huge house in suburbs; nice condo downtown in large city | $$10 million: A small hospital | $$100 million: Large city office building)</span>')
-    .replace(/([\$])([0-9.]+)\s?(billion|bn)/gi, '<span style="color:' + color + '">$1$2 billion (You could buy with: $$1 billion: Burj Khalifa, world\'s tallest building | $$55 billion: Cost of a human mission to Mars with a crew of four | $$299 billion: GDP of Greece)</span>')
-    .replace(/([\$])([0-9.]+) \s?(trillion|tn)/gi, '<span style="color:' + color + '">$1$2 trillion (You could buy with: $$4.79 trillion: United States annual federal budget for 2020 | $$19.49 billion: US GDP in 2017 | $$100 trillion: Global GDP in 2017)</span>')
+function alterElements(tag) {
+
+    var elements = document.getElementsByTagName(tag);
+    for (var i = 0; i < elements.length; i++) {
+        element = elements[i];
+
+ 
+    element.innerHTML = element.innerHTML.replace(/&nbsp;/g, ' ') // NOTE - This might cause a performance hit but I couldn't figure out another way to capture &nbsp;
+        .replace(/([\$|£])([0-9.,]+)(\s?million|m)/gi, millionText)
+        .replace(/([\$|£])([0-9.,]+)(\s?billion|bn)/gi, billionText)
+        .replace(/([\$|£])([0-9.,]+)(\s?trillion|trn)/gi, trillionText)
+        .replace(/([0-9.,]+)(\s?Millionen|Mio\.)\s(Euro|€|Dollar)/gi, millionText2)
+        .replace(/([0-9\.,]+)\s?(Milliarden|Mrd\.)\s(Euro|€|Dollar)/gi, billionText2)
+        .replace(/([0-9.,]+)(\s?Billiaredn|Bln\.)\s(Euro|€|Dollar)/gi, trillionText2)
+    }    
 }
+
+
+alterElements('p', color);
+alterElements('td', color);
+alterElements('li', color);
 
